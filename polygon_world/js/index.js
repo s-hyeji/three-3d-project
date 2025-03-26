2d177ff297e25131127e4130912a168cf58d0581
s-hyeji <shj981004@gamil.com>
2025-03-26 14:14:09 +0900

    3/26 common

--- polygon_world/js/index.js
+++ polygon_world/js/index.js
@@ -2,8 +2,22 @@ import * as THREE from "three";
 import { OrbitControls } from "OrbitControls";
 import gsap from "gsap";
 
-gsap.to("#box", {
- x: 300,
- duration: 2,
- ease: "bounce.out" // 바운스 효과 적용
-});
+// 옵션박스
+const $controller = document.querySelector(".controllerBox");
+
+if ($controller) {
+ let checkbox
+
+ // 체크 박스 설정 
+ const toggleBtns = $controller.querySelectorAll(".controll_toggle");
+ toggleBtns.forEach(function (btn) {
+  btn.addEventListener("change", function () {
+   checkbox = this.querySelector("input");
+   if (checkbox.checked) {
+    console.log("ON");
+   } else {
+    console.log("OFF");
+   }
+  });
+ });
+}
