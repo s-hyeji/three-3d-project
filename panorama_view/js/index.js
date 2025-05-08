fb15ee8f2fd386faf276414deb05a966401bea6c
khwoo91 <168423069+khwoo91@users.noreply.github.com>
2025-05-08 18:16:41 +0900

    5/8 gsap추가

--- panorama_view/js/index.js
+++ panorama_view/js/index.js
@@ -7,14 +7,8 @@ import showShpearMesh from './Mesh/shpearMesh.js';
 import showSquareMesh from './Mesh/squareMesh.js';
 
 
-class Project_view {
-  constructor(root, imgLength) {
-    this.container = document.querySelector(root);
-    this.buttons = this.container.querySelectorAll('button');
-    this.imgLength = imgLength;
-    this.shprearNum = 1;
-    this.squareNum = 1;
-
+class Scene_Event {
+  constructor() {
     // 메쉬 선언
     this.scene = new THREE.Scene();
     this.universeMesh = showUniverse();
@@ -56,8 +50,8 @@ class Project_view {
 
   requestAnimation() {
     requestAnimationFrame(() => this.requestAnimation());
-    let speed = Date.now() * 0.0005;
-    let ratateRadius = 80;
+    // let speed = Date.now() * 0.0005;
+    // let ratateRadius = 80;
     // this.camera.position.x = Math.cos(speed) * ratateRadius;
     // this.camera.position.z = Math.sin(speed) * ratateRadius;
     this.renderer.render(this.scene, this.camera);
@@ -76,11 +70,113 @@ class Project_view {
     });
   }
 
+
+}
+
+
+
+class View_Event {
+  constructor(root, imgLength) {
+    this.container = document.querySelector(root);
+
+    this.startWrap = this.container.querySelector('#start_wrap');
+    this.startWrap_btns = this.container.querySelectorAll('#start_wrap button');
+    this.shpearBtn = this.startWrap.querySelector('#shpear');
+    this.squareBtn = this.startWrap.querySelector('#square');
+
+    this.objectWrap = this.container.querySelector('#object_wrap')
+    this.buttons = this.container.querySelectorAll('button');
+
+
+    this.imgLength = imgLength;
+    this.shprearNum = 1;
+    this.squareNum = 1;
+
+    this.hoverCheck = false;
+    this.leaveCheck = false;
+    this.clickCheck = false;
+
+    this.clickEvent();
+    this.startWrap_btns.forEach((buttons) => { buttons.addEventListener('mouseover', (event) => { this.hoverEvent(event); }); })
+    this.startWrap.addEventListener('mouseleave', (event) => { this.LeaveEvent(event); });
+
+    this.timeLine_H = gsap.timeline();
+    this.timeLine_C = gsap.timeline();
+  }
+
+
+  requestAnimation() {
+    requestAnimationFrame(() => this.requestAnimation());
+    this.orbit.update();
+  }
+
+
+  hoverEvent(event) {
+    this.hoverCheck = true;
+    this.event = event;
+    console.log(this.event, '# 마우스 HOVER!');
+
+    if (!this.hoverCheck) return;
+    if (this.event.target.id === 'shpear') {
+      this.timeLine_H
+        .to(this.shpearBtn, { duration: 0.3, scale: 1.4, x: 190 })
+        .to(this.squareBtn, { duration: 0.3, opacity: 0, y: 200 }, '<')
+        .to(scene_V.shpearMesh.position, { duration: 0.3, x: 0, y: 15, z: 50 }, '<')
+    }
+
+    if (this.event.target.id === 'square') {
+      this.timeLine_H
+        .to(this.squareBtn, { duration: 0.3, scale: 1.4, x: -190 })
+        .to(this.shpearBtn, { duration: 0.3, opacity: 0, y: 200 }, '<')
+        .to(scene_V.squareMesh.position, { duration: 0.3, x: 0, y: 15, z: 48 }, '<')
+    }
+  }
+
+
+  LeaveEvent(event) {
+    this.leaveCheck = true;
+    if (!this.leaveCheck) return;
+    this.event = event;
+    console.log(this.event, '# 마우스 LEAVE!');
+    this.hoverCheck = false;
+
+    this.timeLine_H
+      .to(this.shpearBtn, { duration: 0.3, scale: 1, opacity: 1, x: 0, y: 0 })
+      .to(this.squareBtn, { duration: 0.3, scale: 1, opacity: 1, x: 0, y: 0 }, '<')
+      .to(scene_V.shpearMesh.position, { x: -20, y: 0, z: 0 }, '<')
+      .to(scene_V.squareMesh.position, { x: 20, y: 0, z: 0 }, '<')
+  }
+
+
+
+
   clickEvent() {
     this.buttons.forEach((event) => {
       event.addEventListener('click', (button) => {
         this.button = button;
+        this.hoverCheck = false;
+        console.log(this.button, '# 마우스 CLICK!');
+        
+        // 시작 버튼들
+        if (this.button.target.parentNode.id === 'start_wrap') {
+          this.startWrap.classList.add('displayN');
 
+          if (this.button.target.id === 'shpear') {
+            console.log('# 원 버튼!');
+            setTimeout(() => { this.objectWrap.classList.add('shpear'); }, 2000)
+
+            this.timeLine_C
+              .to(scene_V.shpearMesh.position, { duration: 0.3, x: 0, y: 0, z: 0 }, '0.5')
+              .to(scene_V.squareMesh.position, { duration: 0.7, x: 200, y: 20, z: 0 }, '<')
+              .to(scene_V.camera.position, { duration: 0.5, x: 0, y: 0, z: 0 }, '+1')
+          }
+
+          if (this.button.target.id === 'square') {
+            console.log('# 박스 버튼!');
+          }
+        }
+
+        // 오브젝트들 내 버튼
         if (this.button.target.id === 'prev') {
           console.log('# Prev button!');
           switch (this.container.classList.value) {
@@ -108,54 +204,11 @@ class Project_view {
 
 
 
-class Click_view {
-  constructor() {
-    this.startContaier = document.querySelector('startWrap');
-    this.shpearBtn = document.querySelector('#shpear');
-    this.squareBtn = document.querySelector('#square');
-    project_view.clickEvent();
-    document.addEventListener('mouseover', (event) => { this.hoverEvnet(event); });
 
-    gsap_Ani.animation_01();
-    gsap_Ani.play();
-  }
-
-  hoverEvnet(event) {
-    this.event = event;
-    if (this.event.target.nodeName === 'BUTTON') {
-      if (this.event.target.id === 'shpear') {
-        console.log('# shpear hover!');
-      }
-
-      if (this.event.target.id === 'square') {
-        console.log('# square hover!');
-      }
-    }
-  }
-}
-
-
-
-
-class Gsap_Ani {
-  constructor(root) {
-    this.obj = root
-  }
-
-  animation_01() {
-    console.log('aaaaaaaaa');
-    
-  }
-}
-
-
-
-const project_view = new Project_view('#button_wrap', 5);
-const click_view = new Click_view();
-const gsap_Ani = new Gsap_Ani();
-console.log('# project_view', project_view);
-console.log('# click_view', click_view);
-console.log('# gsap', gsap);
+const scene_V = new Scene_Event();
+const click_E = new View_Event('#wrap', 5);
+console.log('# project_view', scene_V);
+console.log('# click_view', click_E);
 
 
 
