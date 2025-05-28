dc502d3a4854d115acf2aaf115cd5fca30783fdb
khwoo91 <168423069+khwoo91@users.noreply.github.com>
2025-05-28 18:32:03 +0900

    5/28 캐릭터 생성 및 이동

--- three_03_Metaverse/js/index.js
+++ three_03_Metaverse/js/index.js
@@ -2,13 +2,15 @@ import * as THREE from 'three';
 import { OrbitControls } from 'OrbitControls';
 import { GLTFLoader } from "GLTFLoader";
 import { FBXLoader } from "FBXLoader";
-// import { gsap } from 'gsap';
-import { Player } from './object/Player.js';
+import { gsap } from 'gsap';
+import Player from './object/Player.js';
 
 class Scene {
   constructor() {
     this.wrap = document.querySelector('#wrap');
     this.canvasWrap = document.querySelector('#canvasWrap');
+    this.mixerArr = [];
+    this.isClick = true;
     this.init();
   }
 
@@ -19,7 +21,9 @@ class Scene {
 
     // camera
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-    this.camera.position.set(20, 25, 20);
+    console.log(this.camera);
+    
+    this.clock = new THREE.Clock();
 
     // Fog
     // let fogNear = 60;
@@ -32,15 +36,15 @@ class Scene {
     this.canvasWrap.appendChild(this.renderer.domElement);
 
     // lights
-    this.ambientLight = new THREE.AmbientLight("0x404040", 4);
-    this.pointLight = new THREE.PointLight('0x000000', 1);
-    this.pointLight.position.set(10, 30, 10);
-    this.pointLight.castShadow = true;
-    this.pointLight.shadow.mapSize.width = 50;
-    this.pointLight.shadow.mapSize.height = 50;
-    this.pointLight.shadow.radius = 5;
+    this.ambientLight = new THREE.AmbientLight("#eee", 4);
+    // this.pointLight = new THREE.PointLight('#000', 1);
+    // this.pointLight.position.set(10, 30, 10);
+    // this.pointLight.castShadow = true;
+    // this.pointLight.shadow.mapSize.width = 100;
+    // this.pointLight.shadow.mapSize.height = 100;
+    // this.pointLight.shadow.radius = 5;
     this.scene.add(this.ambientLight);
-    this.scene.add(this.pointLight);
+    // this.scene.add(this.pointLight);
 
     // OrbitControls
     this.orbit = new OrbitControls(this.camera, this.renderer.domElement);
@@ -49,25 +53,20 @@ class Scene {
     // this.orbit.enableZoom = false;
     // this.orbit.enableRotate = false;
 
-    this.requestAnimation();
-    this.windowResize();
-    this.initHelper();
+
     this.createObject();
+    this.initHelper();
+    this.windowResize();
+
   }
 
   initHelper() {
     this.scene.add(new THREE.AxesHelper(100));
     this.scene.add(new THREE.GridHelper(100, 20));
-    this.scene.add(new THREE.PointLightHelper(this.pointLight));
+    // this.scene.add(new THREE.PointLightHelper(this.pointLight));
   }
 
-  requestAnimation() {
-    this.camera.lookAt(this.scene.position);
-    this.renderer.render(this.scene, this.camera);
-    this.orbit.update();
 
-    requestAnimationFrame(() => this.requestAnimation());
-  }
 
   windowResize() {
     window.addEventListener('resize', () => {
@@ -83,22 +82,46 @@ class Scene {
       standing: './images/FBX/Standing.fbx',
       running: './images/FBX/Running.fbx',
     }
-    fbxLoader.load(`${player_url.standing}`,
+    fbxLoader.load(
+      player_url.standing,
       (Object) => {
-        new Player(Object, player_url);
+        this.player = new Player(Object, player_url);
         this.scene.add(Object);
+        this.objectMotion();
+        this.requestAnimation();
       },
-      // (xhr) => { console.log((xhr.loaded / xhr.total * 100) + '% loaded'); },
-      // (err) => { console.log(err); }
     )
+  }
+
+  objectMotion() {
+    this.mixer = new THREE.AnimationMixer(this.player.player);
+    this.mixerArr.push(this.mixer);
+    if (this.mixerArr.length > 0) {
+      this.mixer.clipAction(this.player.player.animations[0]).play();
+    }
+  }
+
+  requestAnimation() {
+    this.player.keyboardEvent();
+    for (let i = 0; i < this.mixerArr.length; i++) {
+      this.mixerArr[i].update(this.clock.getDelta());
+    }
+    this.renderer.render(this.scene, this.camera);
+    this.orbit.update();
 
 
-    const planGeometry = new THREE.PlaneGeometry(100, 100);
-    const planMaterial = new THREE.MeshStandardMaterial({ color: "#eee", side: THREE.DoubleSide })
-    const plan = new THREE.Mesh(planGeometry, planMaterial);
-    plan.position.set(0, 0, 0);
-    this.scene.add(plan);
+    // 카메라 이동
+    let c_moveX = this.player.position().moveX;
+    let c_moveZ = this.player.position().moveZ;
+    this.camera.position.set(c_moveX, 40, -30 + c_moveZ);
+    gsap.to(this.camera.position, { x: c_moveX, y: 40, z: -30 + c_moveZ, delay: 0.1 },)
+    // gsap.to(this.camera.lookAt, { x: c_moveX, y: 10, z: 5 + c_moveZ },)
+    this.camera.lookAt(c_moveX, 10, 5 + c_moveZ);
+
+    requestAnimationFrame(() => this.requestAnimation());
   }
 }
+
+
 
 const scene = new Scene();
--- three_03_Metaverse/js/object/Player.js
+++ three_03_Metaverse/js/object/Player.js
@@ -1,16 +1,68 @@
 import * as THREE from 'three';
-import { GLTFLoader } from "GLTFLoader";
+import { FBXLoader } from "FBXLoader";
+import { gsap } from 'gsap';
 
-
-
-
-export class Player {
-  constructor(player, url) {
+export default class Player {
+  constructor(player, src) {
     this.player = player;
-    this.url = url;
+    this.src = src;
+    this.player.scale.set(0.1, 0.1, 0.1);
+    this.moveZ = this.moveX = 0;
+    this.speed = 0.5;
 
     console.log(this.player);
-    this.player.scale.set(0.1, 0.1, 0.1);
-    console.log(this.player.animations[0]);
+    this.player.load()
+  }
+
+  keyboardEvent() {
+
+    if (key.keyDown['up']) {
+      this.player.position.z += this.moveZ + this.speed;
+      gsap.to(this.player.rotation, { y: 0 });
+    }
+
+    if (key.keyDown['down']) {
+      this.player.position.z -= this.moveZ + this.speed;
+      gsap.to(this.player.rotation, { y: 3 });
+    }
+
+    if (key.keyDown['left']) {
+      this.player.position.x += this.moveX + this.speed;
+      gsap.to(this.player.rotation, { y: 1.5 });
+    }
+
+    if (key.keyDown['right']) {
+      this.player.position.x -= this.moveX + this.speed;
+      gsap.to(this.player.rotation, { y: 4.5 });
+      if (key.keyDown['up']) gsap.to(this.player.rotation, { y: 6 });
+    }
+
+    if (key.keyDown['enter']) {
+    }
+
+    if (key.keyDown['spaceBar']) {
+    }
+  }
+
+  position() {
+    return {
+      moveX: this.player.position.x,
+      moveZ: this.player.position.z,
+    }
   }
 }
+
+const key = {
+  keyDown: {},
+  keyValue: {
+    13: 'enter',
+    32: 'spaceBar',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+  },
+}
+
+window.addEventListener('keydown', (e) => { key.keyDown[key.keyValue[e.keyCode]] = true; })
+window.addEventListener('keyup', (e) => { key.keyDown[key.keyValue[e.keyCode]] = false; })
